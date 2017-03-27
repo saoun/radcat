cb005e9e2f31c2dfc10b9dc26d5ba98754cb51ac
Jeff <joelle_van_dyne@joelle-van-dyne.local>
2017-03-27 19:42:55 -0400

    set up localization

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,30 @@
 Rails.application.routes.draw do
-  root to: "home#index"
+  scope "(:locale)", locale: /en|es/ do
+    root to: "home#index"
 
-#home
-  get '/about', to: "home#about"
-  get '/contact', to: "home#contact"
-  get '/wildcats', to: "home#wildcats"
-  get '/panda-security-squad', to: "home#panda-security-squad"
+  #home
+    get '/about', to: "home#about"
+    get '/contact', to: "home#contact"
+    get '/wildcats', to: "home#wildcats"
+    get '/panda-security-squad', to: "home#panda-security-squad"
 
-#projects
-  get '/projects', to: "projects#index"
-  get '/projects/technology-for-social-good-research', to: "projects#technology-for-social-good-research"
-  get '/projects/the-ftx-safety-reboot', to: "projects#the-ftx-safety-reboot"
-  get '/projects/worker-fatality-database', to: "projects#worker-fatality-database"
-  get '/projects/votervox', to: "projects#votervox"
-  get '/projects/who-pays', to: "projects#who-pays"
-  get '/projects/digital-privacy-data-literacy', to: "projects#digital-privacy-data-literacy"
-  get '/projects/media-history-timeline', to: "projects#media-history-timeline"
-  get '/projects/fundraising-and-movement-building-research', to: "projects#fundraising-and-movement-building-research"
-  get '/projects/garment-workers', to: "projects#garment-workers"
-  get '/projects/commslabs', to: "projects#commslabs"
-  get '/projects/rightscon-2015', to: "projects#rightscon-2015"
-  get '/projects/vojo-brasil', to: "projects#vojo-brasil"
-  get '/projects/contratados', to: "projects#contratados"
-  get '/projects/transformative-media-organizing', to: "projects#transformative-media-organizing"
-  get '/projects/out-for-change', to: "projects#out-for-change"
-  get '/projects/media-in-action', to: "projects#media-in-action"
-
+  #projects
+    get '/projects', to: "projects#index"
+    get '/projects/technology-for-social-good-research', to: "projects#technology-for-social-good-research"
+    get '/projects/the-ftx-safety-reboot', to: "projects#the-ftx-safety-reboot"
+    get '/projects/worker-fatality-database', to: "projects#worker-fatality-database"
+    get '/projects/votervox', to: "projects#votervox"
+    get '/projects/who-pays', to: "projects#who-pays"
+    get '/projects/digital-privacy-data-literacy', to: "projects#digital-privacy-data-literacy"
+    get '/projects/media-history-timeline', to: "projects#media-history-timeline"
+    get '/projects/fundraising-and-movement-building-research', to: "projects#fundraising-and-movement-building-research"
+    get '/projects/garment-workers', to: "projects#garment-workers"
+    get '/projects/commslabs', to: "projects#commslabs"
+    get '/projects/rightscon-2015', to: "projects#rightscon-2015"
+    get '/projects/vojo-brasil', to: "projects#vojo-brasil"
+    get '/projects/contratados', to: "projects#contratados"
+    get '/projects/transformative-media-organizing', to: "projects#transformative-media-organizing"
+    get '/projects/out-for-change', to: "projects#out-for-change"
+    get '/projects/media-in-action', to: "projects#media-in-action"
+  end
 end
