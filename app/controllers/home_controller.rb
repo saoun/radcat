b4eea371bf46962e2f6c1fece28394592c370a6b
Jeff <joelle_van_dyne@joelle-van-dyne.local>
2017-03-28 18:18:58 -0400

    add flash and redirect

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,5 +6,7 @@ class HomeController < ApplicationController
     @message = params[:message]
 
     ContactFormMailer.send_email('jeffahking@gmail.com', @name, @email, @message).deliver
+    flash[:notice] = 'Email Sent!'
+    redirect_to :back
   end
 end
